d118c5a4f82e7f42b50cc98c34b11a4443759d1e
khoa-m-doan <khoadoan@uw.edu>
2020-05-02 16:37:02 -0700

    Implement SearchBar.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,36 @@ import youtubeAPI from './api/youtubeAPI';
 {/* Don't need curly brace if it's a default export */}
 
 class App extends React.Component {
+    state = {
+        videos: [],
+        selectedVideo: null,
+    }
+
+    handleSubmit = async (searchTerm) => {
+        {/* async: stop the execution of the code until something is fetched */}
+
+        const response = await youtubeAPI.get('search', { 
+            params: { 
+                part: 'snippet',
+                maxResults: 10,
+                key: 'AIzaSyDhVdg-1pa9rD3lxvIUloPwUIp5d14fnGk',
+                q: searchTerm
+            } 
+        });
+
+       this.setState({
+           videos: response.data.items,
+           selectedVideo: response.data.items[0],
+        });
+    }
+
     render() {
         return (
-            <Grid justify="center" container spacing={16}>
+            <Grid justify="center" container spacing={10}>
                 <Grid item xs={12}>
-                    <Grid container spacing={16}>
+                    <Grid container spacing={10}>
                         <Grid item xs={12}>
-                            <SearchBar />
+                            <SearchBar onFormSubmit={this.handleSubmit}/>
                         </Grid>
                     </Grid>
                     <Grid>
